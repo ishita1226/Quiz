fc4ea3cac8168e19382e711bbf86c46fb4ac8efe
ishita1226 <ishitashukla120603@gmail.com>
2024-04-12 00:10:31 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,7 @@
-
 import 'package:flutter/material.dart';
-import 'package:quiz/start_screen.dart';
 
-void main(){
-  runApp(
-      MaterialApp(
-          home: Scaffold(
-              body: Container(
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [
-                      Color.fromARGB(255, 78, 13, 151),
-                      Color.fromARGB(255, 107, 15, 168),
-                    ],
-                    begin: Alignment.bottomCenter
-                  ),
-                ),
-                  child: const StartScreen())
-          ),
-      )
-  );
+import 'package:quiz/quiz.dart';
+
+void main() {
+  runApp(const Quiz());
 }
