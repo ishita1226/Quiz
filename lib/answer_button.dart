b67079e8404e30567d7a839a411af1ee319d7157
ishita1226 <ishitashukla120603@gmail.com>
2024-04-14 00:40:39 +0530

    Updated.

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -12,23 +12,31 @@ class AnswerButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onTap,
-      style: ElevatedButton.styleFrom(
-        padding: const EdgeInsets.symmetric(
-          vertical: 10,
-          horizontal: 40,
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: onTap,
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 40,
+            ),
+            backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+            foregroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(40),
+            ),
+          ),
+          child: Text(
+            answerText,
+            textAlign: TextAlign.center,
+          ),
         ),
-        backgroundColor: const Color.fromARGB(255, 33, 1, 95),
-        foregroundColor: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(40),
-        ),
-      ),
-      child: Text(
-        answerText,
-        textAlign: TextAlign.center,
-      ),
+        const SizedBox(
+          height: 6,
+        )
+      ],
+
     );
   }
 }
--- lib/questions_summary/questions_summary.dart
+++ lib/questions_summary/questions_summary.dart
@@ -9,7 +9,7 @@ class QuestionsSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int i=1;
+    int i=0;
     return SizedBox(
       height: 400,
       child: SingleChildScrollView(
